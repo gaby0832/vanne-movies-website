8bf434e99be15aef2b3308c0b164753b0b547507
gaby0832 <gdasilvaferreira848@gmail.com>
2026-06-23 13:50:27 -0300

    fix type of usestates

--- app/page.tsx
+++ app/page.tsx
@@ -9,7 +9,7 @@ export default function Home() {
 
 
     const [movies, setMovies] = useState<Array<any>>([]);
-    const [media, setMedia] = useState<Array<any>>([]);
+    const [media, setMedia] = useState<any>(null);
   
 
     useEffect(()=>{
